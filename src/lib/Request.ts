b2b27e7dc966f0bffb8e9b64af3977fc064a7719
luckYaoyao <alex90009@126.com>
2022-08-24 10:15:11 +0800

    feat: Request

--- src/lib/Request.ts
+++ src/lib/Request.ts
@@ -1,3 +1,6 @@
+const METHOD_GET = 'GET'
+const METHOD_POST = 'POST'
+
 interface RequestOption {
     // 请求方法
     method: string
@@ -7,19 +10,33 @@ interface RequestOption {
     data?: Object
 }
 
+const formatParams = (params: any) => {
+    let arr: Array<string> = []
+    for (let key in params) {
+        if (
+            params[key] ||
+            params[key] === 0 ||
+            params[key] === false
+        ) {
+            arr.push(`${key}=${encodeURI(params[key])}`)
+        }
+    }
+    return `${arr.join('&')}`
+}
+
 const Request = function (params: RequestOption) {
     return new Promise((resolve, reject) => {
-        const { method, url } = params
+        let { method, url, data } = params
 
         let xhr = new XMLHttpRequest()
 
+        if (method.toUpperCase() === METHOD_GET) {
+            url += '?' + formatParams(data)
+        }
+
         xhr.open(method, url)
 
-        xhr.setRequestHeader(
-            'Content-Type',
-            'application/x-www-form-urlencoded;charset=UTF-8'
-        )
-
+        setContentType(xhr, 'json')
         xhr.onreadystatechange = function () {
             if (
                 xhr.readyState === 4 &&
@@ -33,13 +50,46 @@ const Request = function (params: RequestOption) {
             reject(event)
         }
 
-        xhr.timeout = 10 * 1000
+        xhr.timeout = 30 * 1000
         xhr.ontimeout = function (event: ProgressEvent) {
             reject(event)
         }
 
-        xhr.send()
+        if (method.toUpperCase() == METHOD_GET) {
+            xhr.send()
+        }
+
+        if (method.toUpperCase() == METHOD_POST) {
+            xhr.send(JSON.stringify(data))
+        }
     })
+}
+
+interface ContentType {
+    json: string
+    form: string
+    text: string
+    html: string
+    [propName: string]: string
+}
+
+const contentTypeList: ContentType = {
+    json: 'application/json; charset=utf-8',
+    form: 'application/x-www-form-urlencoded; charset=utf-8',
+    text: 'text/plain; charset=utf-8',
+    html: 'text/html; charset=utf-8'
+}
+
+const setContentType = (
+    xhr: XMLHttpRequest,
+    type: string
+) => {
+    if (contentTypeList[type]) {
+        xhr.setRequestHeader(
+            'Content-Type',
+            contentTypeList[type]
+        )
+    }
 }
 
 export default Request
